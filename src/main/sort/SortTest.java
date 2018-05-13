9adb9fa52f4ecfeb6d9f728a21c148b07439f362
lxd <768265051@qq.com>
2018-05-13 22:21:43 +0800

    test

--- src/main/sort/SortTest.java
+++ src/main/sort/SortTest.java
@@ -1,45 +1,60 @@
 package main.sort;
 
+import java.util.Arrays;
+
 public class SortTest {
 
-    public static Integer[] testArr = new Integer[]{0, 1, 13, 25, 36, 17, 18, 2};
-
+    public static Integer[] testArr = new Integer[]{10, 1, 13,36, 25,  18,17,  2};
 
 
     public static void main(String[] args) {
 //        bubbleTest(testArr);
-        quickSort(testArr, 0, testArr.length-1);
-        for (Integer s : testArr) {
-            System.out.println(s);
-        }
+        quickSort(testArr, 0, testArr.length - 1);
+        System.out.println(Arrays.toString(testArr));
     }
 
-    private static void quickSort(Integer[] numbers, int start, int end) {
-        int base = numbers[start];
-        int i =start;
-        int j =end;
-        if (j>i){
-            while (base>numbers[i]&&i<end){
-                i++;
-            }
-            while (base<numbers[j]&&j>start){
+    private static void quickSort(Integer[] arr, int low, int high) {
+        System.out.println("low"+low+" hith="+high+"   result="+Arrays.toString(arr));
+        //1,找到递归算法的出口
+        if (low > high) {
+            return;
+        }
+        //2, 存
+        int i = low;
+        int j = high;
+        //3,key
+        int key = arr[low];
+        System.out.println("key ="+key);
+        //4，完成一趟排序
+        while (i < j) {
+            //4.1 ，从右往左找到第一个小于key的数
+            while (i < j && arr[j] > key) {
                 j--;
             }
-            if (i<j){
-                int temp=numbers[i];
-                numbers[i]=numbers[j];
-                numbers[j]=temp;
+            // 4.2 从左往右找到第一个大于key的数
+            while (i < j && arr[i] <= key) {
                 i++;
-                j--;
             }
-            if (i<end){
-                quickSort(numbers,i,end);
-            }
-            if (j>start){
-                quickSort(numbers,start,j);
+            //4.3 交换
+            if (i < j) {
+                int p = arr[i];
+                arr[i] = arr[j];
+                arr[j] = p;
+                System.out.println("change_result="+Arrays.toString(arr));
             }
         }
+        System.out.println("i==="+i);
+//         4.4，调整key的位置
+        int p = arr[i];
+        arr[i] = arr[low];
+        arr[low] = p;
+        System.out.println("调整key"+Arrays.toString(arr));
+        //5, 对key左边的数快排
+        quickSort(arr, low, i - 1);
+        //6, 对key右边的数快排
+        quickSort(arr, i + 1, high);
     }
+}
 
 
 //    public static void bubbleTest(Integer[] numbers){
@@ -54,4 +69,3 @@ public class SortTest {
 //        }
 //    }
 
-}
